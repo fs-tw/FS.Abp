7fb31bb7d8bf2a7bbaef1073d5a79de90dcd6525
YinChang <yincahng0626@gmail.com>
2019-12-23 08:40:42 +0800

    add datamodel

---
+++ src/FS.Cms.Domain.Shared/DataModel/Cms.cs
@@ -0,0 +1,16 @@
+﻿
+
+//暫時不要用#@ extended name="AbpEditor" category="Abp" editor="FS.ED.Editor.AbpEditor,FS.ED" owner="ContextModel" type="System.String" Default="" description="" #>
+
+
+
+
+
+
+
+
+
+
+
+
+
