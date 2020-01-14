ee02ae4f1f47275e3e83b04ea386e6ed4e9226b2
samchen <bcsunrsbh@gmail.com>
2020-01-14 11:04:08 +0800

    update

---
+++ ng-alain/src/lib/post/detail/detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fs-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.less']
+})
+export class DetailComponent implements OnInit {
+  i:any={};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- ng-alain/src/lib/post/post-routing.module.ts
+++ ng-alain/src/lib/post/post-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { MainComponent } from './main/main.component';
-
+import { DetailComponent } from './detail/detail.component';
 const routes: Routes = [
   {
     path: '',
@@ -12,6 +12,10 @@ const routes: Routes = [
         path: '',
         component: MainComponent,
         data: { requiredPolicy: 'AbpIdentity.Roles' },
+      },
+      {
+        path:'detail/:id',
+        component:DetailComponent
       }
     ],
   }
