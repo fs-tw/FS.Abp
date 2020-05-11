00c22f5d079012bf032bda30c791525839873b9b
samchen <bcsunrsbh@gmail.com>
2020-05-11 17:56:10 +0800

    update post  detail

--- ng-alain/src/lib/post/detail/detail.component.ts
+++ ng-alain/src/lib/post/detail/detail.component.ts
@@ -10,8 +10,8 @@ import { AddRoute, ABP, PatchRouteByName, ConfigStateService } from '@abp/ng.cor
 import { NotifyService } from '../../shared/services/notify/notify.service';
 import { PostsStateService } from '../providers/post/poststate.service';
 import { pluck, delay } from 'rxjs/operators';
-import { HttpRequest, HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
+import { HttpRequest, HttpClient, JsonpClientBackend } from '@angular/common/http';
+import * as _ from 'lodash';
 @Component({
   selector: 'fs-detail',
   templateUrl: './detail.component.html',
@@ -35,8 +35,8 @@ export class DetailComponent implements OnInit {
   };
   fileList: any = [];
 
-  showList=[];
-  deleteList=[];
+  showList = [];
+  deleteList = [];
   selectPostId;
   news: any;
   constructor(
@@ -48,6 +48,7 @@ export class DetailComponent implements OnInit {
     private postsStateService: PostsStateService,
     private configStateService: ConfigStateService,
     private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) {
     this.apiUrl = this.configStateService.getApiUrl() + "/";
     this.activatedRoute.params.subscribe(x => {
@@ -55,18 +56,19 @@ export class DetailComponent implements OnInit {
         this.store.dispatch(new GetPostById(x["id"]))
           .pipe(pluck("PostState", "postData"))
           .subscribe(x => {
-            this.i = x;                                    
+            this.i = x;
             this.selectPostId = x["id"];
             this.i.images.forEach((item, index) => {
-                                          
+
               this.showList.push({
                 uid: index.toString(),
                 name: item.title,
                 status: 'done',
                 type: "image/jpeg",
                 url: this.apiUrl + item.url,
-                thumbUrl:this.apiUrl + item.url,
-                needDelete:true                
+                thumbUrl: this.apiUrl + item.url,
+                needDelete: true,
+                isCover: item.isCover
               })
             });
 
@@ -92,12 +94,14 @@ export class DetailComponent implements OnInit {
       url: "",
       content: ""
     };
-    
-    this.showList=[];
-    this.deleteList=[];
-    this.selectPostId="";
-    this.fileList = [];    
+
+    this.showList = [];
+    this.deleteList = [];
+    this.selectPostId = "";
+    this.fileList = [];
     this.i.blogCodeId = this.news[0].id;
+    this.router.navigate(["cms/post"], { queryParams: { blog: this.news[0].id } });
+
   }
 
   ngOnInit() {
@@ -108,51 +112,65 @@ export class DetailComponent implements OnInit {
 
   beforeUpload = (file: any): boolean => {
     //要顯示列表回傳 false 自行處理欲 上傳檔案的列表
-    file.needDelete=false;
+    file.needDelete = false;
     this.fileList.push(file);
     this.showList.push(file);
     return false;
   };
 
   delete(item: any, needDelete: boolean) {
-		if (!needDelete) {
-			var fileListIndex = this.fileList.indexOf(item);
+    if (!needDelete) {
+      var fileListIndex = this.fileList.indexOf(item);
       this.fileList.splice(fileListIndex, 1);
       this.showList.splice(fileListIndex, 1);
-		} else {
-			var saveListIndex = this.showList.indexOf(item);
-			this.deleteList.push(item);
-			this.showList.splice(saveListIndex, 1);
-		}
-	}
-
-  save() {          
+    } else {
+      var saveListIndex = this.showList.indexOf(item);
+      this.deleteList.push(item);
+      this.showList.splice(saveListIndex, 1);
+    }
+  }
+  mapOfCheckedId: { [key: string]: boolean } = {};
+  save() {
     var mes = "建立成功！";
-    if ( this.selectPostId) mes = "更新成功！";
-    this.store.dispatch( this.selectPostId ? new Updatepost(this.i) : new Createpost(this.i))    
-    .pipe(pluck("PostState"))
-    .subscribe(x => {      
-      this.notifyService.success(mes);     
-      if (!this.selectPostId)  this.selectPostId = x.posts.items.filter(y=>y.title==this.i.title)[0].id;
-      this.handleUpload(this.selectPostId);
-      this.initData();
-    })    
-  } 
+    if (this.selectPostId) mes = "更新成功！";
+    
+    var cover = this.showList.filter(x => { return x.isCover == true });    
+    if (cover.length > 0) {
+      this.i.images.forEach(x => {
+        if (x.title != cover[0].name) x.isCover = false;
+        else x.isCover = true;
+      });
+    }
+
+    this.store.dispatch(this.selectPostId ? new Updatepost(this.i) : new Createpost(this.i))
+      .pipe(pluck("PostState"))
+      .subscribe(x => {
+        this.notifyService.success(mes);
+        if (!this.selectPostId) this.selectPostId = x.posts.items.filter(y => y.title == this.i.title)[0].id;
+        this.handleUpload(this.selectPostId);
+        this.initData();
+      })
+  }
 
 
- 
 
   handleUpload(id: string) {
     const formData = new FormData();
     this.fileList.forEach((file: any) => {
-      console.log("run ",file)
       formData.append('files[]', file);
     });
 
     var input = {
-			originFiles: this.showList.map(x => x.name),
-			deleteFiles: this.deleteList.map(x => x.name)
-		}
+      originFiles: this.showList.map(x => x.name),
+      deleteFiles: this.deleteList.map(x => x.name),
+      isCoverName: ""
+    }
+
+    var cover = this.showList.filter(x => { return x.isCover == true });    
+    if (cover.length > 0) {
+      input.isCoverName = cover[0].name;
+    }
+
     formData.append('input', JSON.stringify(input));
 
     const req = new HttpRequest(
@@ -163,13 +181,20 @@ export class DetailComponent implements OnInit {
     );
 
     this.http.request(req).subscribe(
-      (x: any) => {},
-      error => {        
+      (x: any) => { },
+      error => {
         this.notifyService.error('無法上傳');
       }
     );
-
-
   }
 
+  refreshStatus(input) {
+    console.log(this.showList);
+    this.showList.forEach(x => {
+      if (x.uid != input.uid) x.isCover = false;
+    });
+  }
+
+
+ 
 }
