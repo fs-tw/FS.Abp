8b574708d84bf3cd530b15ea2f3844435f0b837d
YinChang <yincahng0626@gmail.com>
2020-05-23 12:42:22 +0800

    fix ci (+5 squashed commit)
    Squashed commit:
    
    [5332271] fix ci
    
    [40ee70f] fix ci
    
    [714f643] fix ci
    
    [3c049a0] fix ci
    
    [4211315] update subtree ci

--- ci/pull-angular-libs.ps1
+++ ci/pull-angular-libs.ps1
@@ -1,4 +1,3 @@
-$ErrorActionPreference = "Stop"
-
-git checkout angular/libs/cms;git fetch;git pull
+$env:GIT_REDIRECT_STDERR = '2>&1'
+git subtree pull -P angular/libs/cms origin angular/libs/cms
 
