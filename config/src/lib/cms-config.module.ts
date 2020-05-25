22a8c31095ca43db36b73ab4eed79c6bf060a9d4
YinChang <yincahng0626@gmail.com>
2020-05-25 19:13:30 +0800

    refactory config.service

--- config/src/lib/cms-config.module.ts
+++ config/src/lib/cms-config.module.ts
@@ -1,8 +1,9 @@
 import { noop } from '@abp/ng.core';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CmsConfigService } from './services/cms-config.service';
+import { InitConfigService } from '@fs/theme.core';
 
 @NgModule({
-  providers: [{ provide: APP_INITIALIZER, deps: [CmsConfigService], useFactory: noop, multi: true }],
+  providers: [{ provide: APP_INITIALIZER, deps: [CmsConfigService], useFactory: InitConfigService, multi: true }],
 })
 export class CmsConfigModule {}
