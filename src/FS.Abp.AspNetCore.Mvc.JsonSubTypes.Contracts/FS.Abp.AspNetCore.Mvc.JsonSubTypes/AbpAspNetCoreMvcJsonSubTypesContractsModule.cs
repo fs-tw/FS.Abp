a745318209dc1eb0974086b5e35ef9ff8a5925cb
YinChang <yincahng0626@gmail.com>
2020-07-21 14:04:50 +0800

    move framework modules path

---
+++ src/FS.Abp.AspNetCore.Mvc.JsonSubTypes.Contracts/FS.Abp.AspNetCore.Mvc.JsonSubTypes/AbpAspNetCoreMvcJsonSubTypesContractsModule.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace FS.Abp.AspNetCore.Mvc.JsonSubTypes
+{
+    public class AbpAspNetCoreMvcJsonSubTypesContractsModule : AbpModule
+    {
+    }
+}
