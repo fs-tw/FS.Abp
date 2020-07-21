5355b915a6b5f8e32d81d8f6482d421813778e1c
YinChang <yincahng0626@gmail.com>
2020-07-21 17:04:41 +0800

    move FS.Abp

---
+++ host/FS.Abp.Core.Host.Shared/Volo.Abp.Identity.HttpApi/AbpIdentityHttpApiHostModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Identity.EntityFrameworkCore;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.Identity.Host.HttpApi
+{
+    [DependsOn(
+        typeof(AbpIdentityApplicationModule),
+        typeof(AbpIdentityHttpApiModule),
+        typeof(AbpIdentityEntityFrameworkCoreModule)
+        )]
+    public class AbpIdentityHttpApiHostModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+    }
+}
---
+++ host/FS.Abp.Core.Host.Shared/Volo.Abp.TenantManagement.HttpApi/AbpTenantManagementHttpApiHostModule.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.TenantManagement.EntityFrameworkCore;
+
+namespace Volo.Abp.TenantManagement.Host.HttpApi
+{
+    [DependsOn(
+        typeof(AbpTenantManagementEntityFrameworkCoreModule),
+        typeof(AbpTenantManagementApplicationModule),
+        typeof(AbpTenantManagementHttpApiModule)
+        )]
+    public class AbpTenantManagementHttpApiHostModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+    }
+}
