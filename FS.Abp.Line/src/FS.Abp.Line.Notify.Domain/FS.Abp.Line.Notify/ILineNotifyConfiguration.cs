108d8b80930365d4f6b017f36f1c8de2f25e4b7b
YinChang <yincahng0626@gmail.com>
2020-07-23 08:52:31 +0800

    add FS.Abp.Line

---
+++ FS.Abp.Line/src/FS.Abp.Line.Notify.Domain/FS.Abp.Line.Notify/ILineNotifyConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace FS.Abp.Line.Notify
+{
+    public interface ILineNotifyConfiguration
+    {
+        Task<string> GetClientIdAsync();
+        Task<string> GetClientSecretAsync();
+        Task<string> GetTokenAsync();
+        Task SetTokenAsync(string token);
+    }
+}
