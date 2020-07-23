74428b5009b5ac96c3cf6685972f5852316513bf
YinChang <yincahng0626@gmail.com>
2020-07-23 08:52:31 +0800

    add FS.Abp.Line

---
+++ src/FS.Abp.Line.Notify.Domain/FS.Abp.Line.Notify/Models/NotifyResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FS.Abp.Line.Notify.Models
+{
+    public class NotifyResponse
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ src/FS.Abp.Line.Notify.Domain/FS.Abp.Line.Notify/Models/StatusRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FS.Abp.Line.Notify.Models
+{
+    public class StatusRequest
+    {
+    }
+}
