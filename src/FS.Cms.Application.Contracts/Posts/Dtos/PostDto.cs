88fd538016841fc703292f99a68b70f125b8b84c
samchen <bcsunrsbh@gmail.com>
2020-08-19 15:23:47 +0800

    postwithDetaildto add coverImage attribute

---
+++ src/FS.Cms.Application.Contracts/Posts/Dtos/PostDto.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Volo.Abp.Application.Dtos;
+using FS.Abp.Application.Dtos;
+using System.Linq;
+
+namespace FS.Cms.Posts.Dtos
+{
+  
+    public partial class PostWithDetailsDto : PostDto
+    {
+        public FS.Cms.Core.Dtos.CmsImageFieldDto CoverImage => Images.Where(x => x.IsCover == true).FirstOrDefault();        
+    }
+   
+}
