fecdd4536cad18756f8e87b06ad60e33644e3dae
ChangYinShung <changyenshung@gmail.com>
2020-09-02 11:00:40 +0800

    add email  contact
    TODO: Contact should add setting

---
+++ src/FS.Cms.Application.Contracts/FS.Cms/Emailing/Dtos/SendMailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FS.Cms.Emailing.Dtos
+{
+    public class SendMailDto
+    {
+        public string TargetEmail { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string TargetName { get; set; }
+    }
+}
