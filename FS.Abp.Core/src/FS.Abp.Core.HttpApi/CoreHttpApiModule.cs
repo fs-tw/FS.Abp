beaf0eba39c801b21905715cbb82c5908380986c
YinChang0626 <yinchang0626@gmail.com>
2020-10-06 18:10:04 +0800

    add dtoWithSettings

--- FS.Abp.Core/src/FS.Abp.Core.HttpApi/CoreHttpApiModule.cs
+++ FS.Abp.Core/src/FS.Abp.Core.HttpApi/CoreHttpApiModule.cs
@@ -4,6 +4,7 @@ using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FS.Abp.Core
 {
@@ -27,6 +28,12 @@ namespace FS.Abp.Core
                 options.Resources
                     .Get<AbpCoreResource>()
                     .AddBaseTypes(typeof(AbpUiResource));
+            });
+
+            Configure<MvcNewtonsoftJsonOptions>(options =>
+            {
+
+                options.SerializerSettings.Converters.Add(new FS.Abp.CodeSettings.DtoWithSettingsConverter());
             });
         }
     }
