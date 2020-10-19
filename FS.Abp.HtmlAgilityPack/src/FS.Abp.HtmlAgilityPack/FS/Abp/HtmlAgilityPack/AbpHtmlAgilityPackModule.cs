469086f66a1dad3b33b5af0bf44fcb67f38acc72
samchen <bcsunrsbh@gmail.com>
2020-10-19 08:30:23 +0800

    add htmlAgilityPack module

---
+++ FS.Abp.HtmlAgilityPack/src/FS.Abp.HtmlAgilityPack/FS/Abp/HtmlAgilityPack/AbpHtmlAgilityPackModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Modularity;
+namespace FS.Abp.RestSharp
+{
+    public class AbpRestSharpModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+    }
+}
+
+
+
