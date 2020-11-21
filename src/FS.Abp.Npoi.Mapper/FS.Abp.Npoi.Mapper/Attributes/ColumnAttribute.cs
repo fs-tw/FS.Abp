0510f73b341d36c36640f7629a8ccd2e9ceb9b3b
YinChang0626 <yinchang0626@gmail.com>
2020-11-21 10:46:12 +0800

    add ReadToTree

---
+++ src/FS.Abp.Npoi.Mapper/FS.Abp.Npoi.Mapper/Attributes/ColumnAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FS.Abp.Npoi.Mapper.Attributes
+{
+    public class LevelStartAtAttribute: Attribute
+    {
+        public LevelStartAtAttribute(ushort index)
+        {
+            Index = index;
+        }
+        //public LevelAttribute(string name)
+        //{
+        //    Name = name;
+        //}
+
+        public int Index { get; }
+        //public string Name { get; }
+    }
+}
--- src/FS.Abp.Npoi.Mapper/FS.Abp.Npoi.Mapper/VirtualFileNpoiReader.cs
+++ src/FS.Abp.Npoi.Mapper/FS.Abp.Npoi.Mapper/VirtualFileNpoiReader.cs
@@ -10,22 +10,9 @@ using NPOI.XSSF.UserModel;
 
 namespace FS.Abp.Npoi.Mapper
 {
-    public class TreeNode
+    public partial class VirtualFileNpoiReader : IVirtualFileNpoiReader
     {
-        public TreeNode()
-        {
-            Children = new List<TreeNode>();
-        }
-        public string No { get; set; }
-        public string Code { get; set; }
-        public string DisplayName { get; set; }
 
-        public string Description { get; set; }
-
-        public List<TreeNode> Children { get; set; }
-    }
-    public class VirtualFileNpoiReader : IVirtualFileNpoiReader
-    {
         private readonly Volo.Abp.VirtualFileSystem.IVirtualFileProvider _virtualFileProvider;
         public VirtualFileNpoiReader(
           Volo.Abp.VirtualFileSystem.IVirtualFileProvider virtualFileProvider
@@ -51,10 +38,101 @@ namespace FS.Abp.Npoi.Mapper
         /// </summary>
         /// <param name="filePath">虛擬檔案路徑</param>
         /// <param name="sheetName">工作簿名稱</param>
-        /// <param name="levelStartIndex">層次欄位開始位置(層次1)</param>
         /// <returns></returns>
-        public List<TreeNode> ReadToTreeNode(string filePath, string sheetName, int levelStartIndex)
+        //public List<TreeNode> ReadToTreeNode(string filePath, string sheetName)
+        //{
+        //    var file = _virtualFileProvider.GetFileInfo(filePath);
+        //    if (!file.Exists)
+        //        return null;
+        //    DataTable dtTable = new DataTable();
+        //    List<string> rowList = new List<string>();
+        //    ISheet sheet;
+        //    Options options = null;
+        //    using (var stream = file.CreateReadStream())
+        //    {
+        //        stream.Position = 0;
+        //        IWorkbook xssWorkbook = WorkbookFactory.Create(stream);
+        //        sheet = xssWorkbook.GetSheet(sheetName);
+        //        options = getOptions(sheet);
+
+        //        IRow headerRow = sheet.GetRow(0);
+        //        int cellCount = headerRow.LastCellNum;
+        //        for (int j = 0; j < cellCount; j++)
+        //        {
+        //            ICell cell = headerRow.GetCell(j);
+        //            if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
+        //            {
+        //                dtTable.Columns.Add(cell.ToString());
+        //            }
+        //        }
+
+        //        for (int i = options.DataStartIndex; i <= sheet.LastRowNum; i++)
+        //        {
+        //            IRow row = sheet.GetRow(i);
+        //            if (row == null) continue;
+        //            if (string.IsNullOrEmpty(row.GetCell(options.LevelStartIndex)?.ToString())) continue;
+        //            for (int j = 0; j < cellCount; j++)
+        //            {
+        //                rowList.Add(row.GetCell(j)?.ToString());
+        //            }
+        //            if (rowList.Count > 0)
+        //                dtTable.Rows.Add(rowList.ToArray());
+        //            rowList.Clear();
+        //        }
+        //    }
+        //    var list = dtTable.AsEnumerable().Select(x =>
+        //    {
+        //        var node = new TreeNode();
+        //        if (x.Table.Columns.Contains("DisplayName"))
+        //        {
+        //            node.DisplayName = x.Field<string>("DisplayName");
+        //        }
+        //        if (x.Table.Columns.Contains("Description"))
+        //        {
+        //            node.Description = x.Field<string>("Description");
+        //        }
+
+        //        node.No = x.Table.Columns.Contains("No") ? x.Field<string>("No") : node.DisplayName;
+
+        //        var codes = x.ItemArray
+        //        .Skip(options.LevelStartIndex)
+        //        .Select(o => o?.ToString()?.PadLeft(4, '0') ?? "0000")
+        //        .Where(o => o != "0000");
+        //        node.Code = string.Join(".", codes);
+        //        return node;
+        //    }).OrderBy(x => x.Code).ToList();
+
+        //    var result = new List<TreeNode>();
+
+        //    list.ForEach(n =>
+        //    {
+        //        if (n.Code.IndexOf('.') == -1)//root
+        //        {
+        //            result.Add(n);
+        //            return;
+        //        }
+
+        //        var parentCode = n.Code.Substring(0, n.Code.LastIndexOf('.'));
+        //        list.Single(t => t.Code == parentCode).Children.Add(n);
+        //    });
+
+
+        //    return result;
+        //}
+
+
+
+    }
+
+    public partial class VirtualFileNpoiReader
+    {
+        public List<T> ReadToTreeNode<T>(string filePath, string sheetName)
+            where T : ITreeNode<T>, new()
         {
+            var attribute = Volo.Abp.Reflection.ReflectionHelper.GetSingleAttributeOrDefault<Npoi.Mapper.Attributes.LevelStartAtAttribute>(typeof(T));
+            if (attribute == null) throw new Exception($"{typeof(T).Name} should has LevelStartAtAttribute");
+            var levelIndex = attribute.Index;
+
             var file = _virtualFileProvider.GetFileInfo(filePath);
             if (!file.Exists)
                 return null;
@@ -66,6 +144,7 @@ namespace FS.Abp.Npoi.Mapper
                 stream.Position = 0;
                 IWorkbook xssWorkbook = WorkbookFactory.Create(stream);
                 sheet = xssWorkbook.GetSheet(sheetName);
+
                 IRow headerRow = sheet.GetRow(0);
                 int cellCount = headerRow.LastCellNum;
                 for (int j = 0; j < cellCount; j++)
@@ -76,10 +155,12 @@ namespace FS.Abp.Npoi.Mapper
                         dtTable.Columns.Add(cell.ToString());
                     }
                 }
+
                 for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++)
                 {
                     IRow row = sheet.GetRow(i);
                     if (row == null) continue;
+                    if (string.IsNullOrEmpty(row.GetCell(levelIndex)?.ToString())) continue;
                     for (int j = 0; j < cellCount; j++)
                     {
                         rowList.Add(row.GetCell(j)?.ToString());
@@ -91,24 +172,24 @@ namespace FS.Abp.Npoi.Mapper
             }
             var list = dtTable.AsEnumerable().Select(x =>
             {
-                var node = new TreeNode();
-                node.DisplayName = x.Field<string>("DisplayName");
-                if (x.Table.Columns.Contains("Description"))
+                var node = new T();
+                typeof(T).GetProperties().ForEach(p =>
                 {
-                    node.Description = x.Field<string>("Description");
-                }
-                
-                node.No = string.IsNullOrEmpty(x.Field<string>("No")) ? node.DisplayName : x.Field<string>("No");
-
+                    var attribute = Volo.Abp.Reflection.ReflectionHelper.GetSingleAttributeOrDefault(p, new global::Npoi.Mapper.Attributes.ColumnAttribute(p.Name));
+                    if (x.Table.Columns.Contains(attribute.Name))
+                    {
+                        p.SetValue(node, x.Field<string>(attribute.Name));
+                    }
+                });
                 var codes = x.ItemArray
-                .Skip(levelStartIndex)
+                .Skip(levelIndex)
                 .Select(o => o?.ToString()?.PadLeft(4, '0') ?? "0000")
                 .Where(o => o != "0000");
                 node.Code = string.Join(".", codes);
                 return node;
             }).OrderBy(x => x.Code).ToList();
 
-            var result = new List<TreeNode>();
+            var result = new List<T>();
 
             list.ForEach(n =>
             {
@@ -126,4 +207,5 @@ namespace FS.Abp.Npoi.Mapper
             return result;
         }
     }
+
 }
