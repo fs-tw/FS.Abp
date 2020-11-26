70de6dca41678016ca0a0a4b46acb27fae88deb7
YinChang0626 <yinchang0626@gmail.com>
2020-11-26 08:32:03 +0800

    add errorhandler

--- src/FS.Abp.Npoi.Mapper/FS.Abp.Npoi.Mapper/VirtualFileNpoiReader.cs
+++ src/FS.Abp.Npoi.Mapper/FS.Abp.Npoi.Mapper/VirtualFileNpoiReader.cs
@@ -12,14 +12,15 @@ namespace FS.Abp.Npoi.Mapper
 {
     public partial class VirtualFileNpoiReader : IVirtualFileNpoiReader
     {
-
         private readonly Volo.Abp.VirtualFileSystem.IVirtualFileProvider _virtualFileProvider;
+        private readonly IErrorHandler errorHandler;
         public VirtualFileNpoiReader(
-          Volo.Abp.VirtualFileSystem.IVirtualFileProvider virtualFileProvider
-
+            Volo.Abp.VirtualFileSystem.IVirtualFileProvider virtualFileProvider,
+            IErrorHandler errorHandler
             )
         {
             this._virtualFileProvider = virtualFileProvider;
+            this.errorHandler = errorHandler;
         }
 
         public List<T> Read<T>(string filePath, string sheetName)
@@ -32,101 +33,11 @@ namespace FS.Abp.Npoi.Mapper
             List<T> sheet = mapper.Take<T>(sheetName).Select(x => x.Value).ToList();
             return sheet;
         }
-
-        /// <summary>
-        /// TreeNode
-        /// </summary>
-        /// <param name="filePath">虛擬檔案路徑</param>
-        /// <param name="sheetName">工作簿名稱</param>
-        /// <returns></returns>
-        //public List<TreeNode> ReadToTreeNode(string filePath, string sheetName)
-        //{
-        //    var file = _virtualFileProvider.GetFileInfo(filePath);
-        //    if (!file.Exists)
-        //        return null;
-        //    DataTable dtTable = new DataTable();
-        //    List<string> rowList = new List<string>();
-        //    ISheet sheet;
-        //    Options options = null;
-        //    using (var stream = file.CreateReadStream())
-        //    {
-        //        stream.Position = 0;
-        //        IWorkbook xssWorkbook = WorkbookFactory.Create(stream);
-        //        sheet = xssWorkbook.GetSheet(sheetName);
-        //        options = getOptions(sheet);
-
-        //        IRow headerRow = sheet.GetRow(0);
-        //        int cellCount = headerRow.LastCellNum;
-        //        for (int j = 0; j < cellCount; j++)
-        //        {
-        //            ICell cell = headerRow.GetCell(j);
-        //            if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
-        //            {
-        //                dtTable.Columns.Add(cell.ToString());
-        //            }
-        //        }
-
-        //        for (int i = options.DataStartIndex; i <= sheet.LastRowNum; i++)
-        //        {
-        //            IRow row = sheet.GetRow(i);
-        //            if (row == null) continue;
-        //            if (string.IsNullOrEmpty(row.GetCell(options.LevelStartIndex)?.ToString())) continue;
-        //            for (int j = 0; j < cellCount; j++)
-        //            {
-        //                rowList.Add(row.GetCell(j)?.ToString());
-        //            }
-        //            if (rowList.Count > 0)
-        //                dtTable.Rows.Add(rowList.ToArray());
-        //            rowList.Clear();
-        //        }
-        //    }
-        //    var list = dtTable.AsEnumerable().Select(x =>
-        //    {
-        //        var node = new TreeNode();
-        //        if (x.Table.Columns.Contains("DisplayName"))
-        //        {
-        //            node.DisplayName = x.Field<string>("DisplayName");
-        //        }
-        //        if (x.Table.Columns.Contains("Description"))
-        //        {
-        //            node.Description = x.Field<string>("Description");
-        //        }
-
-        //        node.No = x.Table.Columns.Contains("No") ? x.Field<string>("No") : node.DisplayName;
-
-        //        var codes = x.ItemArray
-        //        .Skip(options.LevelStartIndex)
-        //        .Select(o => o?.ToString()?.PadLeft(4, '0') ?? "0000")
-        //        .Where(o => o != "0000");
-        //        node.Code = string.Join(".", codes);
-        //        return node;
-        //    }).OrderBy(x => x.Code).ToList();
-
-        //    var result = new List<TreeNode>();
-
-        //    list.ForEach(n =>
-        //    {
-        //        if (n.Code.IndexOf('.') == -1)//root
-        //        {
-        //            result.Add(n);
-        //            return;
-        //        }
-
-        //        var parentCode = n.Code.Substring(0, n.Code.LastIndexOf('.'));
-        //        list.Single(t => t.Code == parentCode).Children.Add(n);
-        //    });
-
-
-        //    return result;
-        //}
-
-
-
     }
 
     public partial class VirtualFileNpoiReader
     {
-        public List<T> ReadToTreeNode<T>(string filePath, string sheetName)
+        public List<T> ReadToTreeNode<T>(string filePath, string sheetName, List<T> errors = null)
             where T : ITreeNode<T>, new()
         {
             var attribute = Volo.Abp.Reflection.ReflectionHelper.GetSingleAttributeOrDefault<Npoi.Mapper.Attributes.LevelStartAtAttribute>(typeof(T));
@@ -198,13 +109,39 @@ namespace FS.Abp.Npoi.Mapper
                     result.Add(n);
                     return;
                 }
-
                 var parentCode = n.Code.Substring(0, n.Code.LastIndexOf('.'));
-                list.Single(t => t.Code == parentCode).Children.Add(n);
+                var availableParents = list.Where(t => t.Code == parentCode);
+                var isExistParent = Validate(n, sheet, availableParents);
+                if (isExistParent)
+                {
+                    list.Single(t => t.Code == parentCode).Children.Add(n);
+                }
+
             });
 
 
             return result;
+        }
+
+        private bool Validate<T>(T n, ISheet sheet, IEnumerable<T> availableParents) where T : ITreeNode<T>, new()
+        {
+            if (availableParents.Count() == 0)
+            {
+                var errorInfo = new ErrorHandler.ErrorInfo()
+                {
+                    Message = $"找不到上層節點，名稱:{n.ToString()},位於：{sheet.SheetName} 工作簿。"
+                };
+                errorHandler.Add(errorInfo);
+            }
+            if (availableParents.Count() > 1)
+            {
+                var errorInfo = new ErrorHandler.ErrorInfo()
+                {
+                    Message = $"找到多個上層節點，名稱:{n.ToString()},位於：{sheet.SheetName} 工作簿。"
+                };
+                errorHandler.Add(errorInfo);
+            }
+            return availableParents.Count() == 1;
         }
     }
 
