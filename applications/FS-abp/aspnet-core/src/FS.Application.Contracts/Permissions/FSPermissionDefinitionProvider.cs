f51203133bd69c10841edbe22affb642c1ebbe52
jennyhu2020 <jennyhu2020@test.com>
2020-12-08 18:42:28 +0800

    add prj

---
+++ applications/FS-abp/aspnet-core/src/FS.Application.Contracts/Permissions/FSPermissionDefinitionProvider.cs
@@ -0,0 +1,22 @@
+﻿using FS.Localization;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Localization;
+
+namespace FS.Permissions
+{
+    public class FSPermissionDefinitionProvider : PermissionDefinitionProvider
+    {
+        public override void Define(IPermissionDefinitionContext context)
+        {
+            var myGroup = context.AddGroup(FSPermissions.GroupName);
+
+            //Define your own permissions here. Example:
+            //myGroup.AddPermission(FSPermissions.MyPermission1, L("Permission:MyPermission1"));
+        }
+
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<FSResource>(name);
+        }
+    }
+}
---
+++ applications/FS-abp/aspnet-core/src/FS.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleToAttribute("FS.Domain.Tests")]
+[assembly:InternalsVisibleToAttribute("FS.TestBase")]
