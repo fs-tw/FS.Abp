4dbcb08d8a96cfcdba7d43a4d03a441d2736442b
YinChang0626 <yinchang0626@gmail.com>
2020-12-20 23:22:53 +0800

    update all

--- ci/common.ps1
+++ ci/common.ps1
@@ -5,7 +5,11 @@ $rootFolder = (Get-Item -Path "./" -Verbose).FullName
 # List of solutions
 $solutionPaths = (
     "..\framework",
-    "..\modules\Volo",
+    "..\framework\Volo.Abp",
+
+    "..\modules\FS.Theme\FS.Theme.NgAlainMs",
+    "..\modules\FS.Theme\FS.Theme.TheProject",
+    "..\modules\FS.Theme\FS.Theme.Unify",
 
     "..\applications\FS\aspnet-core"
     
