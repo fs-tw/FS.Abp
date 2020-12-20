5c682a949ca48f0ba6fd684e3c2828e3dbac5e5e
YinChang0626 <yinchang0626@gmail.com>
2020-12-20 16:56:17 +0800

    Init version abp 4.0.2

---
+++ src/FS.Abp.Domain/FS.Abp.Settings/Factory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.Settings;
+using Volo.Abp.Threading;
+
+namespace FS.Abp.Settings
+{
+    public interface IFactory<T>
+        where T : class, new()
+    {
+        T Value { get; }
+        Task UpdateAsync(T input, string providerName, string providerKey);
+    }
+    public abstract class Factory<T> : IFactory<T>
+        where T : class, new()
+    {
+        public ISettingManager SettingManager { get; set; }
+        public ISettingProvider SettingProvider { get; set; }
+        public virtual T Value
+        {
+            get
+            {
+                var result = new T();
+                AsyncHelper.RunSync(() => CreateAsync(result));
+                return result;
+            }
+        }
+
+        public abstract Task CreateAsync(T options);
+        protected virtual async Task SetAsync(string name,string value, string providerName, string providerKey)
+        {
+            if (string.IsNullOrEmpty(providerName))
+            {
+                await SettingManager.SetGlobalAsync(name, value);
+            }
+            else
+            {
+                await SettingManager.SetAsync(name, value, providerName, providerKey);
+            }
+        }
+
+        public abstract Task UpdateAsync(T input, string providerName, string providerKey);
+    }
+}
