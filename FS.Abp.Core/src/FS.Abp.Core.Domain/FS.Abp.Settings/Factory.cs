6712e0c26e82412f192569c60c7d18883849cee9
hong <Further-PC@DESKTOP-TSTKTDU>
2020-12-28 16:19:13 +0800

    Abp.Settings Factory update

--- FS.Abp.Core/src/FS.Abp.Core.Domain/FS.Abp.Settings/Factory.cs
+++ FS.Abp.Core/src/FS.Abp.Core.Domain/FS.Abp.Settings/Factory.cs
@@ -12,35 +12,38 @@ namespace FS.Abp.Settings
         where T : class, new()
     {
         //Task<T> CreateAsync();
-        T Value { get; }
-        Task UpdateAsync(T domain, string providerName, string providerKey);
+        //T Value { get; }
+        //Task UpdateAsync(T domain, string providerName, string providerKey);
+
+        Task<T> GetAsync(string providerName, string providerKey);
+        Task SetAsync(T input, string providerName, string providerKey);
     }
-    public abstract class Factory<T> : IFactory<T>
-        where T : class, new()
-    {
-        protected readonly ISettingProvider _settingProvider;
+    //public abstract class Factory<T> : IFactory<T>
+    //    where T : class, new()
+    //{
+    //    protected readonly ISettingProvider _settingProvider;
 
-        public Factory(
-            ISettingProvider settingProvider
-            )
-        {
-            _settingProvider = settingProvider;
-        }
-        public virtual T Value
-        {
-            get
-            {
-                var result = new T();
-                AsyncHelper.RunSync(() => CreateAsync(result));
-                return result;
-            }
-        }
+    //    public Factory(
+    //        ISettingProvider settingProvider
+    //        )
+    //    {
+    //        _settingProvider = settingProvider;
+    //    }
+    //    public virtual T Value
+    //    {
+    //        get
+    //        {
+    //            var result = new T();
+    //            AsyncHelper.RunSync(() => CreateAsync(result));
+    //            return result;
+    //        }
+    //    }
 
-        protected abstract Task CreateAsync(T options);
+    //    protected abstract Task CreateAsync(T options);
 
-        public virtual Task UpdateAsync(T domain, string providerName, string providerKey)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public virtual Task UpdateAsync(T domain, string providerName, string providerKey)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
