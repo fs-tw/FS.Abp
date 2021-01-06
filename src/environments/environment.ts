5b90d7daf52a58889063ce416b3e6e2d9664c593
YinChang0626 <yinchang0626@gmail.com>
2021-01-06 10:36:05 +0800

    add GFLA app and appt datamodel

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'GFLA',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44368',
+    redirectUri: baseUrl,
+    clientId: 'GFLA_App',
+    responseType: 'code',
+    scope: 'offline_access GFLA',
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44368',
+      rootNamespace: 'GFLA',
+    },
+  },
+} as Environment;
