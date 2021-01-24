762614f12cae02520265bd4f5154b0b4c202dcb7
YinChang0626 <yinchang0626@gmail.com>
2021-01-24 12:20:22 +0800

    add angular theme-unify lib

---
+++ angular/libs/theme-unify/src/lib/providers/initial.provider.ts
@@ -0,0 +1,37 @@
+import { ApplicationRef, APP_INITIALIZER, ElementRef, Injector } from '@angular/core';
+import { Store, Actions, ofActionDispatched } from '@ngxs/store';
+import { RouterNavigation } from '@ngxs/router-plugin';
+import { Router, NavigationEnd } from '@angular/router';
+import { LayoutStateService } from '../service/layout-state.service';
+import { of } from 'rxjs';
+import { delay, filter } from 'rxjs/operators';
+//import { GetTheme, RouterState, UpdateProfile, eThemeNos } from '@fs/theme.core';
+
+
+export const UNIFY_THEME_INITIAL_PROVIDERS = [
+  {
+    provide: APP_INITIALIZER,
+    useFactory: configureInitial,
+    deps: [Injector],
+    multi: true,
+  },
+];
+
+export function configureInitial(injector: Injector) {
+  return () => {
+    const router = injector.get(Router);
+    const layoutStateService = injector.get(LayoutStateService);
+    initial(router,layoutStateService);
+  };
+}
+
+function initial( router: Router, layoutStateService: LayoutStateService) {
+  router.events
+    .pipe(
+      filter(e => e instanceof NavigationEnd),
+      delay(0))
+    .subscribe(event => {
+      console.log('angular-ready');
+      layoutStateService.AppComponent.nativeElement.dispatchEvent(new CustomEvent('angular-ready'));
+    });
+}
---
+++ angular/libs/theme-unify/src/lib/theme-unify.module.ts
@@ -0,0 +1,84 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CoreModule } from '@abp/ng.core';
+import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { UNIFY_THEME_STYLES_PROVIDERS } from './providers/styles.provider';
+import { UNIFY_THEME_INITIAL_PROVIDERS } from './providers/initial.provider';
+import { ApplicationLayoutComponent } from './components/application-layout/application-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AccountLayoutComponent } from './components/account-layout/account-layout.component';
+import { EmptyLayoutComponent } from './components/empty-layout/empty-layout.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxValidateCoreModule } from '@ngx-validate/core';
+import { ValidationErrorComponent } from './components/validation-error/validation-error.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { UNIFY_THEME_SETTING_TAB_PROVIDERS } from './providers/setting-tab.provider';
+
+export const LAYOUTS = [ApplicationLayoutComponent, AccountLayoutComponent, EmptyLayoutComponent];
+@NgModule({
+  declarations: [
+    ...LAYOUTS,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    ValidationErrorComponent,
+    SettingsComponent
+
+  ],
+  exports: [
+    ...LAYOUTS,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent
+  ],
+  entryComponents: [
+    ...LAYOUTS
+  ],
+
+  imports: [
+    CoreModule,
+    ThemeSharedModule, 
+    NgbDropdownModule, 
+    NgxValidateCoreModule
+  ]
+})
+export class ThemeUnifyModule {
+  static forRoot(): ModuleWithProviders<ThemeUnifyModule> {
+    return {
+      ngModule: RootUnifyModule,
+      providers: [
+        UNIFY_THEME_STYLES_PROVIDERS,
+        UNIFY_THEME_INITIAL_PROVIDERS,
+        UNIFY_THEME_SETTING_TAB_PROVIDERS
+      ],
+    };
+  }
+}
+
+@NgModule({
+  imports: [
+    NgxValidateCoreModule.forRoot({
+      targetSelector: '.form-control',
+      invalidClasses: 'input-validation-error',
+      blueprints: {
+        creditCard: 'AbpValidation::ThisFieldIsNotAValidCreditCardNumber.',
+        email: 'AbpValidation::ThisFieldIsNotAValidEmailAddress.',
+        invalid: 'AbpValidation::ThisFieldIsNotValid.',
+        max: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+        maxlength:
+          'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMaximumLengthOf{0}[{{ requiredLength }}]',
+        min: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+        minlength:
+          'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMinimumLengthOf{0}[{{ requiredLength }}]',
+        ngbDate: 'AbpValidation::ThisFieldIsNotValid.',
+        passwordMismatch: 'AbpIdentity::Volo.Abp.Identity:PasswordConfirmationFailed',
+        range: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+        required: 'AbpValidation::ThisFieldIsRequired.',
+        url: 'AbpValidation::ThisFieldIsNotAValidFullyQualifiedHttpHttpsOrFtpUrl',
+      },
+      errorTemplate: ValidationErrorComponent,
+    })
+  ],
+})
+export class RootUnifyModule { }
