ad68f2b69fa86d50c8dba407e585231da0cc0543
pei02 <57085184+pei02@users.noreply.github.com>
2021-03-12 10:31:51 +0800

    update theme default

--- angular/libs/theme/config/src/default/routedefinition/default-routedefinition-entity-actions.ts
+++ angular/libs/theme/config/src/default/routedefinition/default-routedefinition-entity-actions.ts
@@ -1,31 +1,31 @@
-// import { EntityAction } from '@abp/ng.theme.shared/extensions';
-// import { Fs } from '@fs-tw/cms/proxy';
-// import { ExtensionsService } from '../../services/extensions.service';
-// import { eThemeRouteNames } from '../../enums/route-names';
+import { EntityAction } from '@abp/ng.theme.shared/extensions';
+import { Fs } from 'libs/theme/proxy/src';
+import { ExtensionsService } from '../../services/extensions.service';
+import { eThemeRouteNames } from '../../enums/route-names';
 
-// export const DEFAULT_ROUTEDEFINITION_ENTITY_ACTIONS = EntityAction.createMany<
-//   Fs.Cms.Blogs.Dtos.BlogDto
-// >([
-//   {
-//     text: 'AbpIdentity::Edit',
-//     action: (data) => {
-//       const service = data.getInjected(ExtensionsService);
-//       service.action(eThemeRouteNames.RouteDefinition, {
-//         name: 'Edit',
-//         record: data.record,
-//       });
-//     },
-//     //permission: 'AbpIdentity.Users.Update',
-//   },
-//   {
-//     text: 'AbpIdentity::Delete',
-//     action: (data) => {
-//       const service = data.getInjected(ExtensionsService);
-//       service.action(eThemeRouteNames.RouteDefinition, {
-//         name: 'Delete',
-//         record: data.record,
-//       });
-//     },
-//     //permission: 'AbpIdentity.Users.Delete',
-//   },
-// ]);
+export const DEFAULT_ROUTEDEFINITION_ENTITY_ACTIONS = EntityAction.createMany<
+  Fs.Theme.Routes.Dtos.RouteDefinitionDto
+>([
+  {
+    text: 'AbpIdentity::Edit',
+    action: (data) => {
+      const service = data.getInjected(ExtensionsService);
+      service.action(eThemeRouteNames.RouteDefinition, {
+        name: 'Edit',
+        record: data.record,
+      });
+    },
+    //permission: 'AbpIdentity.Users.Update',
+  },
+  {
+    text: 'AbpIdentity::Delete',
+    action: (data) => {
+      const service = data.getInjected(ExtensionsService);
+      service.action(eThemeRouteNames.RouteDefinition, {
+        name: 'Delete',
+        record: data.record,
+      });
+    },
+    //permission: 'AbpIdentity.Users.Delete',
+  },
+]);
