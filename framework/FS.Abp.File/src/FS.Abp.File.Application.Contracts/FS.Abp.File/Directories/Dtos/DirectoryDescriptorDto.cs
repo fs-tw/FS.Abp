3f4b3f123e4235d6ad8916e49219b07601224b98
samchen <bcsunrsbh@gmail.com>
2021-03-16 18:07:42 +0800

    update proxy

--- framework/FS.Abp.File/src/FS.Abp.File.Application.Contracts/FS.Abp.File/Directories/Dtos/DirectoryDescriptorDto.cs
+++ framework/FS.Abp.File/src/FS.Abp.File.Application.Contracts/FS.Abp.File/Directories/Dtos/DirectoryDescriptorDto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Volo.Abp.Application.Dtos;
 
 namespace FS.Abp.File.Directories.Dtos
 {
-    public class DirectoryDescriptorDto : Volo.FileManagement.Directories.DirectoryDescriptorDto
+    public class DirectoryDescriptorDto : AuditedEntityDto<Guid>
     {
+        public string Name { get; set; }
+        public Guid? ParentId { get; set; }
         public DirectoryDescriptorDto Parent { get; set; }
     }
 }
