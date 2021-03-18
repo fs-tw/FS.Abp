75a86932f161a2b24b37bc627d99f9665a269bd3
ChangYinShung <changyenshung@gmail.com>
2021-03-18 10:02:49 +0800

    Resolves #31

--- angular/libs/cms/config/src/enums/route-names.ts
+++ angular/libs/cms/config/src/enums/route-names.ts
@@ -6,10 +6,10 @@ export const enum eCmsRouteNames {
   Post = 'Cms::FS.Cms.PostManagement',
   Post_Detail = 'Cms::FS.Cms.PostDetail',
   Post_Detail_Id = 'Cms::FS.Cms.PostDetail.Id',
-  Sample = 'Cms::Fs.Cms.Sample',
-  Storage = 'Cms::Fs.Cms.Storage',
+  Sample = 'Cms::FS.Cms.Sample',
+  Storage = 'Cms::FS.Cms.Storage',
   // PageSetting = '網站基本配置'
-  Tag='標籤維護',
-  Tag_detail='標籤詳細'
+  Tag='Cms::FS.Tag.Management',
+  Tag_detail='FS.Tag.Management.Detail'
 
 }
