f3b75d32b7396f20e2a631c6476727136894aae9
pei02 <57085184+pei02@users.noreply.github.com>
2021-03-22 16:42:55 +0800

    add announcement

--- angular/apps/unify-public/src/app/app-routing.module.ts
+++ angular/apps/unify-public/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+  },
+   {
+   path: 'announcement',
+   loadChildren: () => import('./announcement/announcement.module').then(m => m.AnnouncementModule),
   }
   //{
   //  path: 'identity',
