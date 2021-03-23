7db6f60a508a60a2c2678a2968682ba1f9589aeb
yinchang0626 <yincahng0626@gmail.com>
2021-03-23 15:16:55 +0800

    fix pageNav when no route match

--- angular/libs/theme-alain-ms/layout/src/lib/ms/shared/service-layout/service-layout.component.ts
+++ angular/libs/theme-alain-ms/layout/src/lib/ms/shared/service-layout/service-layout.component.ts
@@ -12,10 +12,10 @@ export class MSServiceLayoutComponent {
   @Input() @InputBoolean() nav = false;
   @Input() navConfig: MSServiceNavConfig = {};
   @Input() navList: MSMenu[] = [];
-  @Input() hasConsoleCss:boolean = true;
+  @Input() hasConsoleCss: boolean = true;
 
   get hideNav(): boolean {
-    return this.srv.hideNav;
+    return this.srv.hideNav || this.nav === false || this.navList.length === 0;
   }
 
   constructor(private srv: BrandService) {}
