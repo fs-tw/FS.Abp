e4892345c67a0b67658612e33d3c51996154129b
yinchang0626 <yincahng0626@gmail.com>
2021-03-23 16:32:59 +0800

    add Google External Login

---
+++ framework/FS.Abp.Authentication/src/FS.Abp.Authentication.External.Google/AuthenticationExternalGoogleModule.cs
@@ -0,0 +1,26 @@
+﻿using FS.Abp.Authentication.External;
+using Volo.Abp.Domain;
+using Volo.Abp.Modularity;
+
+namespace FS.Abp.Authentication.External.Google
+{
+    [DependsOn(
+        typeof(AuthenticationDomainModule)
+    )]
+    public class AuthenticationExternalGoogleModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+
+            Configure<ExternalAuthProviderOptions>(options =>
+            {
+                options.ExternalAuthProviders.AddOrReplace(
+                    new ExternalAuthProviderConfiguration(
+                    "Google",
+                    typeof(GoogleExternalAuthProvider)
+                    ));
+
+            });
+        }
+    }
+}
