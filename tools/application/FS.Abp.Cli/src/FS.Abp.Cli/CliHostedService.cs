f4864ab1e70e6fd4300228ffbc672bc060e8f24f
yinchang0626 <yinchang0626@gmail.com>
2021-04-14 10:57:34 +0800

    add FS.Abp.Cli to rename project

---
+++ tools/application/FS.Abp.Cli/src/FS.Abp.Cli/CliHostedService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+
+namespace FS.Abp.Cli
+{
+    public class CliHostedService : IHostedService
+    {
+        private readonly IAbpApplicationWithExternalServiceProvider _application;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly RenameService _renameService;
+
+        public CliHostedService(
+            IAbpApplicationWithExternalServiceProvider application,
+            IServiceProvider serviceProvider,
+            RenameService renameService)
+        {
+            _application = application;
+            _serviceProvider = serviceProvider;
+            _renameService = renameService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _application.Initialize(_serviceProvider);
+
+            _renameService.Execute();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _application.Shutdown();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ tools/application/FS.Abp.Cli/src/FS.Abp.Cli/RenameService.cs
@@ -0,0 +1,73 @@
+﻿using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.IO;
+using Volo.Abp.Cli.ProjectBuilding;
+using Volo.Abp.Cli.ProjectBuilding.Building;
+using Volo.Abp.Cli.ProjectBuilding.Building.Steps;
+using Volo.Abp.DependencyInjection;
+
+namespace FS.Abp.Cli
+{
+    public class RenameService : ITransientDependency
+    {
+
+        private readonly string sourceZipPath = @"D:\Works\11003\ec.works-0129\modules\FS.EShop\FS.EShop.zip";
+        private readonly string oldSolutionName = "FS.EShop";
+        private readonly string newSolutionName = "FS.EShopManagement";
+        private readonly string destPath = @$"C:\";
+        public void Execute()
+        {
+            var context = new ProjectBuildContext(
+                 null,
+                 null,
+                 new TemplateFile(File.ReadAllBytes(sourceZipPath), "", "", ""),
+                 new ProjectBuildArgs(SolutionName.Parse(newSolutionName))
+             );
+
+            var getFiles = new FileEntryListReadStep();
+            getFiles.Execute(context);
+
+            var rename = new FS.Abp.Cli.ProjectBuilding.Building.Steps.SolutionRenameStep(SolutionName.Parse(oldSolutionName));
+            rename.Execute(context);
+
+            var zip = new CreateProjectResultZipStep();
+            zip.Execute(context);
+
+
+
+            using (var templateFileStream = new MemoryStream(context.Result.ZipContent))
+            {
+                using (var zipInputStream = new ZipInputStream(templateFileStream))
+                {
+                    var zipEntry = zipInputStream.GetNextEntry();
+                    while (zipEntry != null)
+                    {
+                        var fullZipToPath = Path.Combine(destPath, newSolutionName, zipEntry.Name);
+                        var directoryName = Path.GetDirectoryName(fullZipToPath);
+
+                        if (!string.IsNullOrEmpty(directoryName))
+                        {
+                            Directory.CreateDirectory(directoryName);
+                        }
+
+                        var fileName = Path.GetFileName(fullZipToPath);
+                        if (fileName.Length == 0)
+                        {
+                            zipEntry = zipInputStream.GetNextEntry();
+                            continue;
+                        }
+
+                        var buffer = new byte[4096]; // 4K is optimum
+                        using (var streamWriter = File.Create(fullZipToPath))
+                        {
+                            StreamUtils.Copy(zipInputStream, streamWriter, buffer);
+                        }
+
+                        zipEntry = zipInputStream.GetNextEntry();
+                    }
+                }
+            }
+        }
+    }
+}
