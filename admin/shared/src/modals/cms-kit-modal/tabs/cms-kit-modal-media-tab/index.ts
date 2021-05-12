4ac87c35a81d1a79ca7a2d584767a511c14bee2e
yinchang0626 <yinchang0626@gmail.com>
2021-05-12 15:08:08 +0800

    no message

---
+++ admin/shared/src/modals/cms-kit-modal/tabs/cms-kit-modal-media-tab/index.ts
@@ -0,0 +1,1 @@
+export * from './cms-kit-modal-media-tab.component';
---
+++ admin/shared/src/modals/cms-kit-modal/tabs/index.ts
@@ -0,0 +1,4 @@
+export * from './cms-kit-modal-tab.component';
+export * from './cms-kit-modal-blog-feature-tab/index';
+export * from './cms-kit-modal-info-tab/index';
+export * from './cms-kit-modal-media-tab/index';
