1e83776214e342769e07c28d1f96f36d2cf5eee0
yinchang0626 <yinchang0626@gmail.com>
2021-06-24 11:49:15 +0800

    add FS.Abp.MediatR

---
+++ framework/FS.Abp.MediatR/src/FS.Abp.MediatR/FS.Abp.MediatR/AbpMediatRModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using Volo.Abp.Modularity;
+using System.Text;
+using System;
+
+namespace FS.Abp.MediatR
+{
+    [DependsOn(typeof(FS.Abp.AspNetCore.Mvc.JsonSubTypes.AbpAspNetCoreMvcJsonSubTypesAbstractionsModule))]
+    public class AbpMediatRModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+        }
+
+    }
+
+}
