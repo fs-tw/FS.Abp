1cce0684cadd4b94321ef6bc6f458991c5a92a7d
Hong <skywalking17@gmail.com>
2021-06-28 13:45:11 +0800

    fix route seed

--- modules/FS.CmsKitManagement/src/FS.CmsKitManagement.Domain/Data/Routes/RoutesSeeder.cs
+++ modules/FS.CmsKitManagement/src/FS.CmsKitManagement.Domain/Data/Routes/RoutesSeeder.cs
@@ -33,41 +33,91 @@ namespace FS.CmsKitManagement.Data.Routes
         {
             var definition = context.GetProperty<RouteDefinition>()[this.PublicDefinitionNo];
 
+            var existDataNos = await this.AsyncExecuter.ToListAsync(this.RoutesStore.Route
+                .Where(x => !x.ParentId.HasValue)
+                .Select(x => x.No));
+
             var sheetNames = this.VirtualFileNpoiReader.GetSheetNames(Options.FileName);
             foreach (var sheetName in sheetNames)
             {
-                var items = this.VirtualFileNpoiReader.ReadToTreeNode<RoutesInfo>(Options.FileName, sheetName);
-                var entities = mapToEntity<RoutesInfo, Route>(items);
-                //var vocabularys = items.Select(i => mapTo(definition.Id, null, i));
+                var itemTrees = this.VirtualFileNpoiReader.ReadToTreeNode<RoutesInfo>(Options.FileName, sheetName);
+                itemTrees = itemTrees.Where(x => !existDataNos.Contains(x.No)).ToList(); // 僅新增目前不存在的 No
 
-                //await this.VocabulariesStore.VocabularyDefinition.InsertAsync(definition, true);
-                //await this.VocabulariesStore.Vocabulary.InsertManyAsync(vocabularys, true);
+                var entityTrees = mapToEntity<RoutesInfo, Route>(itemTrees, null);
+
+                var items = this.treeToList(itemTrees);
+                var entities = this.treeToList(entityTrees);
+
+                foreach (var entity in entities)
+                {
+                    var item = items.SingleOrDefault(x => x.Code == entity.Code);
+                    if (item == null) continue;
+
+                    entity.No = item.No;
+                    entity.RouteDefinitionId = definition.Id;
+                    entity.UrlType = UrlType.內部連結;
+                    entity.IsHidden = false;
+                    entity.TenantId = this.CurrentTenant.Id;
+                }
+
+                await this.RoutesStore.Route.InsertManyAsync(entityTrees);
+                existDataNos = existDataNos.Concat(entityTrees.Select(x => x.No).ToList()).ToList();
             };
         }
 
-        protected List<TEntity> mapToEntity<TInfo, TEntity>(List<TInfo> datas)
-            where TInfo : ITreeNode<TInfo>
+        protected List<TEntity> mapToEntity<TInfo, TEntity>(List<TInfo> datas, Guid? parentId)
+            where TInfo : ITreeInfo<TInfo>
             where TEntity : class, ITree<TEntity>, new()
         {
-            var result = new List<TEntity>();
+            var result = infoTreeToEntityList(datas, parentId);
 
             return result;
 
-            List<TEntity> infoTreeToEntityList(List<TInfo> datas)
+            List<TEntity> infoTreeToEntityList(List<TInfo> datas, Guid? parentId)
             {
                 var result = new List<TEntity>();
                 foreach(var item in datas)
                 {
                     TEntity entity = new TEntity();
+                    entity.DisplayName = item.DisplayName;
+                    entity.Code = item.Code;
+                    entity.ParentId = parentId;
+                    
                     EntityHelper.TrySetId(entity, this.GuidGenerator.Create);
+
+                    entity.Children = infoTreeToEntityList(item.Children, entity.Id);
+
+                    result.Add(entity);
                 }
                 return result;
             }
         }
+
+        protected List<TTree> treeToList<TTree>(List<TTree> tree)
+            where TTree : class
+        {
+            var result = new List<TTree>();
+            foreach (var item in tree)
+            {
+                result.Add(item);
+
+                var childrenProperty = typeof(TTree).GetProperty("Children").GetValue(item).As<List<TTree>>();
+                var children = this.treeToList(childrenProperty);
+                result = result.Concat(children).ToList();
+            }
+
+            return result;
+        }
+    }
+
+    public interface ITreeInfo<T> : ITreeNode<T>
+        where T : ITreeNode<T>
+    {
+        public string DisplayName { get; set; }
     }
 
     [LevelStartAt(2)]
-    public class RoutesInfo : ITreeNode<RoutesInfo>
+    public class RoutesInfo : ITreeInfo<RoutesInfo>
     {
         [Column("代碼")]
         public string No { get; set; }
