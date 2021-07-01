9bce171d91dadd6421dd21674b911eec91e1df01
Xin <f109193102@nkust.edu.tw>
2021-07-01 12:24:00 +0800

    fix EfCoreEntityExtensionMappings

---
+++ modules/FS.CmsKitManagement/src/FS.CmsKitManagement.EntityFrameworkCore/EntityFrameworkCore/CmsKitManagementEfCoreEntityExtensionMappings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.ObjectExtending;
+using Volo.Abp.Threading;
+
+namespace FS.CmsKitManagement.EntityFrameworkCore
+{
+    public static class CmsKitManagementEfCoreEntityExtensionMappings
+    {
+        private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
+
+        public static void Configure()
+        {
+            OneTimeRunner.Run(() =>
+            {
+                ObjectExtensionManager.Instance.MapEfCoreProperty<Volo.CmsKit.Blogs.BlogPost, int>(Consts.ViewCount);
+                ObjectExtensionManager.Instance.MapEfCoreProperty<Volo.CmsKit.Blogs.Blog, string>(Consts.DisplayStyle);
+                ObjectExtensionManager.Instance.MapEfCoreProperty<Volo.CmsKit.Pages.Page, string>(Consts.DisplayStyle);
+            });
+        }
+    }
+}
