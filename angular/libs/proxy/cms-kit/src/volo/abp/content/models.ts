d9e9b215b45dc9d1df77edd2e3d30c38b367632a
yinchang0626 <yinchang0626@gmail.com>
2021-09-03 10:47:31 +0800

    update angular to abp 4.4.2

--- angular/libs/proxy/cms-kit/src/volo/abp/content/models.ts
+++ angular/libs/proxy/cms-kit/src/volo/abp/content/models.ts
@@ -2,9 +2,11 @@
 export interface IRemoteStreamContent {
   contentType?: string;
   contentLength?: number;
+  fileName?: string;
 }
 
 export interface RemoteStreamContent {
   contentType?: string;
   contentLength?: number;
+  fileName?: string;
 }
--- angular/libs/proxy/cms-kit/src/volo/cms-kit/admin/index.ts
+++ angular/libs/proxy/cms-kit/src/volo/cms-kit/admin/index.ts
@@ -1,6 +1,7 @@
 import * as Blogs from './blogs';
 import * as Comments from './comments';
 import * as MediaDescriptors from './media-descriptors';
+import * as Menus from './menus';
 import * as Pages from './pages';
 import * as Tags from './tags';
-export { Blogs, Comments, MediaDescriptors, Pages, Tags };
+export { Blogs, Comments, MediaDescriptors, Menus, Pages, Tags };
