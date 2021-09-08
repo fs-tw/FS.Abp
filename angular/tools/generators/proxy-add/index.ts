7b481357845c7fb3ffb09f4ea8507d57977324ed
yinchang0626 <yinchang0626@gmail.com>
2021-09-08 16:38:50 +0800

    update proxy-add command for add proxy to module library path

--- angular/tools/generators/proxy-add/index.ts
+++ angular/tools/generators/proxy-add/index.ts
@@ -11,35 +11,38 @@ import { libraryGenerator } from '@nrwl/workspace/generators';
 import { wrapAngularDevkitSchematic } from '@nrwl/devkit/ngcli-adapter';
 
 export default async function (host: Tree, schema: any) {
+  let module=schema.module;
+  let source=schema.application;
+  let target=schema.library;
   let runAngularLibrarySchematic = wrapAngularDevkitSchematic(
     '@abp/ng.schematics',
     'proxy-add'
   );
 
   await runAngularLibrarySchematic(host, {
-    module: schema.module,
-    source: schema.source,
-    target: 'proxy',
+    module: module,
+    source: source,
+    target: target
   });
 
-  console.log(`proxy add '${schema.module} success`);
+  console.log(`proxy add '${module} to ${target} success at ${source}`);
 
   moveFilesToNewDirectory(
     host,
-    'libs\\proxy\\src\\lib\\proxy',
-    `libs\\proxy\\${schema.module}\\src`
+    `libs\\${target}\\src\\lib\\proxy`,
+    `libs\\${target}\\proxy\\${module}\\src`
   );
 
   generateFiles(
     host,
     joinPathFragments(__dirname, '.', './files'),
-    `libs\\proxy\\${schema.module}`,
+    `libs\\${target}\\proxy\\${module}`,
     {
       tmpl: '',
     }
   );
 
   return () => {
-    console.log(`proxy add '${schema.module} success`);
+    console.log(`proxy add '${module} to ${target} success at ${source}`);
   };
 }
