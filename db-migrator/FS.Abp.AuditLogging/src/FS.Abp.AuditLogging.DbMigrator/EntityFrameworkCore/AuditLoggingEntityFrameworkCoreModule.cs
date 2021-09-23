655ae70437c15c3334f98b6fe239847b03b6bd9e
yinchang0626 <yinchang0626@gmail.com>
2021-09-23 08:18:55 +0800

    add DbMigrator for AuditLogging and use postgres database

---
+++ db-migrator/FS.Abp.AuditLogging/src/FS.Abp.AuditLogging.DbMigrator/EntityFrameworkCore/AuditLoggingEntityFrameworkCoreModule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Modularity;
+
+namespace FS.Abp.AuditLogging.EntityFrameworkCore
+{
+    [DependsOn(typeof(AbpAuditLoggingEntityFrameworkCoreModule))]
+    public class AuditLoggingEntityFrameworkCoreModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAbpDbContext<AuditLoggingDbContext>(options =>
+            {
+                /* Remove "includeAllEntities: true" to create
+                 * default repositories only for aggregate roots */
+                options.AddDefaultRepositories(includeAllEntities: true);
+            });
+
+            Configure<AbpDbContextOptions>(options =>
+            {
+                /* The main point to change your DBMS.
+                 * See also AuditLoggingMigrationsDbContextFactory for EF Core tooling. */
+                //options.UseSqlServer();
+                //options.UseNpgsql();
+                options.Configure<AuditLoggingDbContext>(o => o.UseNpgsql());
+            });
+        }
+    }
+}
