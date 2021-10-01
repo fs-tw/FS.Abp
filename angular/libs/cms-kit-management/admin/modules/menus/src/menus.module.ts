c1b77eb98e956a2a2196e0e34e9f626cf4a95c15
kao <kao@test.com>
2021-10-01 16:48:23 +0800

    add abptree

--- angular/libs/cms-kit-management/admin/modules/menus/src/menus.module.ts
+++ angular/libs/cms-kit-management/admin/modules/menus/src/menus.module.ts
@@ -5,13 +5,14 @@ import { CmsKitManagementSharedModule } from '@fs-tw/cms-kit-management/shared';
 import { PickCoverImageModule } from '@fs-tw/components/pick-cover-image';
 import { MenusComponent } from './components/menus/menus.component';
 import { MenusRoutingModule } from './menus-routing.module';
-
+import { TreeModule } from '@abp/ng.components/tree';
 @NgModule({
   declarations: [MenusComponent],
   imports: [
     MenusRoutingModule,
     CmsKitManagementSharedModule,
     PickCoverImageModule,
+    TreeModule
   ],
 })
 export class MenusModule {}
