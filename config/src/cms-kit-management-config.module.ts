03cfbb5339226a459cbc1a6d42cb31c170c94780
yinchang0626 <yinchang0626@gmail.com>
2021-10-16 12:06:12 +0800

    (angular) refactory multi-lingual

--- config/src/cms-kit-management-config.module.ts
+++ config/src/cms-kit-management-config.module.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CMS_KIT_MANAGEMENT_ROUTE_PROVIDERS } from './providers/route.provider';
-import { EntityTypeService } from '@fs-tw/entity-type-management/config';
-import { MULTI_LINGUAL_CONFIG_OPTIONS } from '@fs-tw/components/multi-lingual/config';
+import { EntityTypeStore } from '@fs-tw/entity-type-management/config';
+import { MULTI_LINGUAL_ENTITY_TYPE_STORE } from '@fs-tw/components/multi-lingual';
 
 @NgModule({
   imports: [
@@ -16,8 +16,8 @@ export class CmsKitManagementConfigModule {
       providers: [
         CMS_KIT_MANAGEMENT_ROUTE_PROVIDERS,
         {
-          provide: MULTI_LINGUAL_CONFIG_OPTIONS,
-          useClass: EntityTypeService
+          provide: MULTI_LINGUAL_ENTITY_TYPE_STORE,
+          useExisting: EntityTypeStore
         }
       ],
     };
