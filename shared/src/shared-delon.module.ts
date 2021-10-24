0ca31851a4894f17dcfda8b24b735e268c8e85b9
yinchang0626 <yinchang0626@gmail.com>
2021-10-24 17:08:05 +0800

    replace extensible-form with nz

--- shared/src/shared-delon.module.ts
+++ shared/src/shared-delon.module.ts
@@ -3,13 +3,13 @@
 import { SEModule } from '@delon/abc/se';
 // import { STModule } from '@delon/abc/st';
 // import { SVModule } from '@delon/abc/sv';
-import { DelonFormModule } from '@delon/form';
+//import { DelonFormModule } from '@delon/form';
 
 export const SHARED_DELON_MODULES = [
     // STModule, 
     // SVModule, 
     SEModule,
-    DelonFormModule
+    //DelonFormModule
     // PageHeaderModule,
     // ResultModule
 ];
