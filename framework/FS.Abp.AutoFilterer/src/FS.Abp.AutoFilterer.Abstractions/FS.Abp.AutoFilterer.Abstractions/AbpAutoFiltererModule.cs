1b7387ea8ed6848eceb66b6688070845180f1452
yinchang0626 <yinchang0626@gmail.com>
2021-11-08 10:36:35 +0800

    publish FS.Abp.EntityTypes and update version to 443.0.2

--- framework/FS.Abp.AutoFilterer/src/FS.Abp.AutoFilterer.Abstractions/FS.Abp.AutoFilterer.Abstractions/AbpAutoFiltererModule.cs
+++ framework/FS.Abp.AutoFilterer/src/FS.Abp.AutoFilterer.Abstractions/FS.Abp.AutoFilterer.Abstractions/AbpAutoFiltererModule.cs
@@ -1,12 +1,4 @@
-﻿using AutoFilterer.Swagger;
-using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Volo.Abp.Modularity;
+﻿using Volo.Abp.Modularity;
 
 namespace FS.Abp.AutoFilterer.Abstractions
 {
