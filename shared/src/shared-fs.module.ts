83a91a6e158c4866f1a41a87b097a3f7492f82e3
kao <kao@test.com>
2021-11-09 09:42:43 +0800

    add short entity type get function

--- shared/src/shared-fs.module.ts
+++ shared/src/shared-fs.module.ts
@@ -3,7 +3,6 @@ import { ModalModule } from '@fs-tw/components/modals';
 import { PageModule } from '@fs-tw/components/page';
 import { MultiLingualModule } from '@fs-tw/components/multi-lingual';
 import { ImagePickerModule } from '@fs-tw/components/image-picker';
-import { ThemeQuillEditorModule } from '@fs-tw/components/quill-editor';
 
 export const SHARED_FS_MODULES = [
     ThemeAlainUiExtensionsModule,
@@ -11,5 +10,4 @@ export const SHARED_FS_MODULES = [
     ModalModule,
     MultiLingualModule,
     ImagePickerModule,
-    ThemeQuillEditorModule
 ];
