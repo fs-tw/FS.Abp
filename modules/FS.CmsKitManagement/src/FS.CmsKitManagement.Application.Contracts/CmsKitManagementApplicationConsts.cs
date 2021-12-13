16525dd8364d3fc87946f0819440f2de66c80e16
yinchang0626 <yinchang0626@gmail.com>
2021-12-13 14:21:06 +0800

    add CmsKitManagementConsts

---
+++ modules/FS.CmsKitManagement/src/FS.CmsKitManagement.Application.Contracts/CmsKitManagementApplicationConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace FS.CmsKitManagement
+{
+    public static class CmsKitManagementApplicationConsts
+    {
+        public const string RemoteServiceName = "cms-kit-management";
+        public const string AreaName = "cms-kit-management";
+    }
+}
