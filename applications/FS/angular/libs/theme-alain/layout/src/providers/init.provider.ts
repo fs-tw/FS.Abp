c07439459f7d11297655bdda611a02a45abe7fcf
yinchang0626 <yinchang0626@gmail.com>
2021-12-25 19:54:47 +0800

    5.0.0 angular modify  alain routes

--- applications/FS/angular/libs/theme-alain/layout/src/providers/init.provider.ts
+++ applications/FS/angular/libs/theme-alain/layout/src/providers/init.provider.ts
@@ -1,15 +1,13 @@
 import {
   RoutesService,
   LocalizationPipe,
-  PermissionService,
-  eLayoutType,
   EnvironmentService,
+  TreeNode,
+  ABP,
 } from '@abp/ng.core';
-import { APP_INITIALIZER, inject, Injector } from '@angular/core';
+import { APP_INITIALIZER, Injector } from '@angular/core';
 import { ResolveEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
-//import { LayoutStateService } from '../services/layout-state.service';
-import { eThemeSharedRouteNames } from '@abp/ng.theme.shared';
+import { filter, tap } from 'rxjs/operators';
 import { MenuService, Menu } from '@delon/theme';
 
 export const LAYOUT_INIT_PROVIDERS = [
@@ -28,78 +26,46 @@ export function init(injector: Injector) {
 }
 
 export function listenRouter(injector: Injector) {
+  let transToMenu = function (route: TreeNode<ABP.Route>): Menu {
+    let menu: Menu = {
+      id: route.name,
+      text: localizationPipe.transform(route.name),
+      icon: route.iconClass,
+      children: route.children
+        .filter((r) => !routesService.hide(r))
+        .map((c) => transToMenu(c)),
+      link: route.path,
+    };
+    
+    return menu;
+  };
+
   const router = injector.get(Router);
-  //const layoutStateService = injector.get(LayoutStateService);
   const routesService = injector.get(RoutesService);
   const menuService = injector.get(MenuService);
   const localizationPipe = injector.get(LocalizationPipe);
   const environmentService = injector.get(EnvironmentService);
 
-  routesService.visible$
+  routesService.tree$
     .pipe(
-      filter(
-        (x) =>
-          x.find((y) => y.name === eThemeSharedRouteNames.Administration)
-            ?.children?.length > 0
-      ),
-      map((x) => {
-        return x
-          .filter(
-            (y) =>
-              y.name === eThemeSharedRouteNames.Administration &&
-              !routesService.hide(y)
-          )
-          .map((r) => r.children)
-          .reduce((a, b) => a.concat(b))
-          .map((r) => {
-            let node: Menu = {
-              id: r.name,
-              text: localizationPipe.transform(r.name),
-              icon: r.iconClass,
-              children: [],
-              link: r.path,
-            };
-            node.children = r.children
-              .filter((c) => !routesService.hide(c))
-              .map((c) => {
-                let children: Menu = {
-                  text: localizationPipe.transform(c.name),
-                  link: c.path,
-                  icon: c.iconClass,
-                  id: c.name,
-                  children:c.children.filter((g) => !routesService.hide(g)).map(g=>{
-                    return {
-                      text: localizationPipe.transform(g.name),
-                      link: g.path,
-                      icon: g.iconClass,
-                      id: g.name,
-                    } as Menu;
-                  } )
-                };
-                return children;
-              });
+      tap((roots) => {
+        menuService.clear();
 
-            return node;
-          });
+        let menus = roots
+          .filter((r) => !routesService.hide(r))
+          .map(transToMenu);
+
+        let root: Menu = {
+          text: environmentService.getEnvironment().application?.name,
+          children: menus,
+        };
+
+        menuService.add([root]);
       })
     )
-    .subscribe((menus) => {
-      menuService.clear();
-      let root: Menu = {
-        text: environmentService.getEnvironment().application?.name,
-        children: menus,
-      };
-      menuService.add([root]);
-    });
-
-  routesService.visible$.subscribe((r) => {});
+    .subscribe();
 
   router.events
     .pipe(filter((event): event is ResolveEnd => event instanceof ResolveEnd))
-    .subscribe((event) => {
-      //const currentUrl = decodeURI(event.state.url.split('?')[0]);
-      //layoutStateService.setStore({ currentUrl });
-      //layoutStateService.fetchPageNavs(event.state);
-      routesService.patch('AbpAccount::Login', { layout: eLayoutType.account });
-    });
+    .subscribe((event) => {});
 }
